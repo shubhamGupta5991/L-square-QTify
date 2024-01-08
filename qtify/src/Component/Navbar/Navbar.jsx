56e109fa73155f562f1e1d5322acc9eab3b08818
Shubham Gupta <guptashubham5991@gmail.com>
2024-01-08 17:10:12 +0530

    little changes made in folders

--- qtify/src/Component/Navbar/Navbar.jsx
+++ qtify/src/Component/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Brand from '../Brand_icon/Brand';
-import Search_Bar from '../Search_bar/Search';
+import Search from '../Search_bar/Search';
 import Button from '../Button/Button';
 function Navbar() {
   return (
     <div>
       <Brand/>
-      <Search_Bar/>
+      <Search/>
       <Button/>
     </div>
   )
